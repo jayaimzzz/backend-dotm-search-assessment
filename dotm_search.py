387e35952ef42b965e40663411aa0dbfc0c309b5
jayaimzzz <jameshorton@gmail.com>
2019-02-13 16:28:45 -0500

    dotm file is unzipping

--- dotm_search.py
+++ dotm_search.py
@@ -5,12 +5,22 @@
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "jayaimzzz"
 
+import os
+from zipfile import ZipFile
 
 def main():
-    raise NotImplementedError("Your awesome code begins here!")
-
+    path = "./dotm_files/"
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith("dotm"):
+            print file
+            with ZipFile(path + file, "r") as zip:
+                # zip.printdir()
+                data = zip.read("word/document.xml")
+                print data
+    
 
 if __name__ == '__main__':
     main()
