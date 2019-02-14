72558d3e59da6d0179ef3e0e152ba6ba06653530
jayaimzzz <jameshorton@gmail.com>
2019-02-14 14:14:30 -0500

    works but only when searching for one single charactor. does not work when searching for a word

--- dotm_search.py
+++ dotm_search.py
@@ -11,22 +11,38 @@ import re
 import os
 from zipfile import ZipFile
 from lxml import etree
+import argparse
+import sys
 
-def main():
-    path = "./dotm_files/"
+def main(directory, search_text):
+    path = directory + "/" if directory != None else "./"
     files = os.listdir(path)
-    for file in files:
-        if file.endswith("dotm"):
-            print file
-            with ZipFile(path + file, "r") as zip:
-                # zip.printdir()
-                print zip.
-                # print dir(zip)
-                # text_start_tag_locations = [l.start() for l in re.finditer("</w:t>", xml)]
-                # i = xml.find("<w:t>")
-                # print text_start_tag_locations
-                # print xml
-    
+    files_matched = 0
+    files_searched = 0
+    print "Searching director {} for text '{}' ...".format(path,search_text)
+    for fil in files:
+        if fil.endswith("dotm"):
+            files_searched += 1
+            with ZipFile(path + fil, "r") as zip:
+                for zippedfile in zip.namelist():
+                    if zippedfile.endswith("xml"):
+                        text = zip.read(zippedfile)
+                        if search_text in text:
+                            files_matched += 1
+                            print "Match found in file {}{}".format(path,fil)
+                            len1 = len(text)
+                            for i, c in enumerate(text):
+                                if c == search_text:
+                                    start_i = 0 if i < 40 else i - 40
+                                    end_i = len1 if i + 40 > len1 else i + 40
+                                    print "   ...{}...".format(text[start_i:end_i])
+    print "Total dotm files searched: {}".format(files_searched)
+    print "Total dotm files matched: {}".format(files_matched)
 
 if __name__ == '__main__':
-    main()
+    """Search a directory of dotm files for text"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir",help="directory to search")
+    parser.add_argument("search_text", help="the text to search for")
+    args = parser.parse_args()
+    main(args.dir,args.search_text)
